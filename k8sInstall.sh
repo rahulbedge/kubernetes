1043b7d8437c777a46f6193b5f7a1fb018fc95df
Rahul Bedge <rahul.bedge@hotmail.com>
2023-05-21 15:09:17 +1000

    Added kubeadm init

--- k8sInstall.sh
+++ k8sInstall.sh
@@ -7,12 +7,17 @@ sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = tru
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
 
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
 br_netfilter
 EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
 
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
 EOF
 
 sudo sysctl --system
@@ -24,3 +29,6 @@ sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://dl.k8s
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update -y
 sudo apt install -y kubeadm kubectl kubelet
+
+sudo kubeadm init
+sudo scp ./admin.conf root@192.168.1.10:/root/admin.co
