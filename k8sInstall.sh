73a60df014c3bf65aeb613952ac5591508ba46c9
Rahul Bedge <rahul.bedge@hotmail.com>
2023-05-21 18:25:00 +1000

    add sudo hwclock --hctosys

--- k8sInstall.sh
+++ k8sInstall.sh
@@ -1,3 +1,4 @@
+sudo hwclock --hctosys 
 sudo apt update -y && sudo apt dist-upgrade -y
 sudo swapoff -a 
 sudo apt install -y containerd
@@ -7,16 +8,12 @@ sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = tru
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
 
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-overlay
 br_netfilter
 EOF
 
-sudo modprobe overlay
 sudo modprobe br_netfilter
 
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF
 
