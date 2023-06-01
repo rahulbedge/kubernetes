8b7b7d3b4d5e2e2c20c90d56dbd19035b696d4d3
Rahul Bedge <rahul.bedge@hotmail.com>
2023-06-01 18:33:08 +1000

    Debian K8s installer script

---
+++ debian-k8s-install.sh
@@ -0,0 +1,51 @@
+clear
+apt update -y && sudo apt dist-upgrade -y
+swapoff -a 
+apt-get install ca-certificates curl gnupg -y
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+   tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+docker run hello-world
+
+# Run these commands as root
+###Install GO###
+mkdir -p /root/installer/cri
+cd /root/installer/cri
+wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
+tar -C /root/installer/cri -xzf go1.20.4.linux-amd64.tar.gz
+git clone https://github.com/Mirantis/cri-dockerd.git
+cd cri-dockerd
+mkdir bin
+/root/installer/cri/go/bin/go build -o bin/cri-dockerd
+mkdir -p /usr/local/bin
+install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
+cp -a packaging/systemd/* /etc/systemd/system
+sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+systemctl daemon-reload
+systemctl enable cri-docker.service
+systemctl enable --now cri-docker.socket
+cd /root/installer
+rm -rf ./cri
+
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt update -y
+apt install -y kubeadm kubectl kubelet
+
+kubeadm init --apiserver-advertise-address=172.168.1.12 --pod-network-cidr=10.244.0.0/16 --cri-socket=unix:///var/run/cri-dockerd.sock --v=5
+
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/calico.yaml -O
+kubectl apply -f calico.yaml
+
+cd /root
+rm -rf go/ installer/
